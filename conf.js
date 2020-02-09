5c6610f9bd91ab5d664f214c74b2f16a75ecf06a
朱俊青10012229 <10012229@zte.intra>
2020-02-09 10:35:39 +0800

    reinit

--- conf.js
+++ conf.js
@@ -1,11 +1,13 @@
 jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
-  ver: '73',
+  // 每次修改配置，该值需要增加，否则不会生效。
+  // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
+  ver: '118',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 18
+    ver: 79
   },
 
   // 节点配置
@@ -36,15 +38,7 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
-        // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
-
-        // 免费版（低权重，分摊一些成本）
-        // 每个账号每天 10 万次免费请求，但有频率限制
-        'a.007.workers.dev': 1,
-        'a.hehe.workers.dev': 1,
-        'a.lulu.workers.dev': 1,
-        'shrill-unit-8594.jsproxy.workers.dev': 1,
+        'node-cfworker-2.etherdream.com': 1,
       }
     }
   },
@@ -53,7 +47,7 @@ jsproxy_config({
    * 默认节点
    */
   // node_default: 'mysite',
-  node_default: /jsproxy\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -64,10 +58,16 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  assets_cdn: 'assets/',
+  // assets_cdn: 'assets/',
+
+  // 首页路径
+  index_path: 'index_v3.html',
+
+  // 支持 CORS 的站点列表（实验中...）
+  direct_host_list: 'cors_v1.txt',
 
   /**
    * 自定义注入页面的 HTML
