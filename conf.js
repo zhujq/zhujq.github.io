fefafff04fffdde18d0db13be4d1705958ee2051
朱俊青10012229 <10012229@zte.intra>
2020-02-09 14:42:02 +0800

    new

--- conf.js
+++ conf.js
@@ -38,12 +38,12 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'ray.zhujq.workers.dev': 4,
+        'ray.zjqzjq.workers.dev': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         
-        'ray.zhujq.workers.dev': 1,
+        'ray.zjqzjq.workers.dev': 1,
       }
     }
   },
